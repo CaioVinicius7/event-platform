3a82373b7f4db0facc31f0d03e4134729404ba8a
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-22 23:09:45 -0300

    feat: Sistema de rotas para vídeos completo

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,16 +6,63 @@ import {
   Image,
   Lightning
 } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
 import "@vime/core/themes/default.css";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video({ lessonSlug }: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: lessonSlug
+    }
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p> Carregando... </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="KJj70dBgRPo" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -24,32 +71,26 @@ export function Video() {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
 
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
-              rerum eum! Repudiandae assumenda alias, impedit ad illum quibusdam
-              rem ab nostrum neque animi quas accusamus nulla nisi labore
-              temporibus aperiam!
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6 ">
               <img
-                src="https://github.com/caiovinicius7.png"
+                src={data.lesson.teacher.avatarURL}
                 alt="teacher"
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
               />
 
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block ">
-                  Caio Vinícius
+                  {data.lesson.teacher.name}
                 </strong>
 
                 <span className="text-gray-200 text-sm block">
-                  {" "}
-                  Desenvolvedor Backend{" "}
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
