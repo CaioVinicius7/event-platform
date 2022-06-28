f10e1018369a4deaa4937b06a3a7d2e64bf9de73
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-28 09:58:29 -0300

    feat: Responsividade da página Subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -4,24 +4,24 @@ import imgUrl from "../assets/code-mockup.png";
 export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+      <div className="w-full max-w-[1100px] flex flex-col lg:flex-row items-center justify-between mt-20 sm:px-8 md:px-4">
+        <div className="flex flex-col items-center lg:items-start justify-center max-w-[640px] lg:max-w-[610px] text-center md:text-start md:pr-1">
           <Logo />
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="mt-8 text-[2.5rem] leading-tight px-4 sm:px-0">
             Construa uma
             <strong className="text-blue-500"> aplicação completa</strong>, do
             zero, com <strong className="text-blue-500"> React </strong>
           </h1>
 
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-4 text-gray-200 leading-relaxed px-4 sm:px-0">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded mt-10 w-full md:w-[480px] lg:w-[355px]">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
@@ -49,7 +49,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src={imgUrl} alt="code-mockup" className="mt-10" />
+      <img src={imgUrl} alt="code-mockup" className="mt-4 lg:mt-10" />
     </div>
   );
 }
